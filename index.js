efc44ac0e48e6bd57053d2dd9c77c0c1eef3dbc8
Zach <zach@aconitum.local>
2024-02-10 12:03:35 -0700

    Added html styles and js content

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-index.js
+const toggleBtn = document.getElementById('toggleBtn');
+const toggleIcon = document.getElementById('toggleIcon');
+
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        toggleIcon.innerHTML = '<i class="fas fa-moon"></i>';
+    } else {
+        toggleIcon.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+});
